c9e152734fef896ccac67d82d4b2aaf4e36ce000
hieunm2 <hieunm2@kaopiz.com>
2025-01-17 11:14:12 +0700

    Task 2

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,9 +9,11 @@ import { Vector as VectorSource } from "ol/source.js";
 import { Vector as VectorLayer } from "ol/layer.js";
 import { Type } from "ol/geom/Geometry";
 import { SimpleGeometry } from "ol/geom";
-import { style } from "../libs/style";
-import { selectClick, selectDoubleClick } from "../libs/select";
+import { selectedStyle, style } from "../libs/style";
 import { useTypeContext } from "../context/TypeContext";
+import Select from "ol/interaction/Select";
+import { click } from "ol/events/condition";
+import interactionDoubleClickZoom from "ol/interaction/DoubleClickZoom";
 
 const MapComponent = ({ type }: { type: string }) => {
   const { setType } = useTypeContext();
@@ -21,6 +23,7 @@ const MapComponent = ({ type }: { type: string }) => {
   const drawRef = useRef<Draw | null>(null); // Reference to store the draw instance
   const modifyRef = useRef<Modify | null>(null); // Reference to store the modify instance
   const snapRef = useRef<Snap | null>(null); // Reference to store the snap instance
+  const selectInteractionRef = useRef<Select | null>(null);
 
   useEffect(() => {
     if (mapRef.current && !mapInstance.current) {
@@ -35,48 +38,21 @@ const MapComponent = ({ type }: { type: string }) => {
         style: style,
       });
 
+      const highlightSource = new VectorSource({ wrapX: false });
+      const highlightVector = new VectorLayer({
+        source: highlightSource,
+        style: selectedStyle,
+      });
+
       sourceRef.current = source;
 
       mapInstance.current = new Map({
         target: mapRef.current,
-        layers: [raster, vector],
+        layers: [raster, vector, highlightVector],
         view: new View({
           center: [0, 0],
           zoom: 2,
         }),
-      });
-
-      mapInstance.current.addInteraction(selectClick);
-      mapInstance.current.addInteraction(selectDoubleClick);
-
-      modifyRef.current = new Modify({
-        source: source,
-      });
-
-      mapInstance.current.addInteraction(modifyRef.current);
-
-      // selectDoubleClick.on("select", () => {
-      //   if (selectDoubleClick.getFeatures().getLength() > 0) {
-      //     document.body.style.cursor = "move"; // Đổi con trỏ khi có feature được chọn
-      //   } else {
-      //     document.body.style.cursor = "default";
-      //   }
-      // });
-
-      modifyRef.current.on("modifystart", () => {
-        if (mapInstance.current) {
-          mapInstance.current.removeInteraction(selectClick);
-          mapInstance.current.removeInteraction(selectDoubleClick);
-        }
-        document.body.style.cursor = "move";
-      });
-
-      modifyRef.current.on("modifyend", () => {
-        if (mapInstance.current) {
-          mapInstance.current.addInteraction(selectClick);
-          mapInstance.current.addInteraction(selectDoubleClick);
-        }
-        document.body.style.cursor = "default";
       });
 
       const draw = new Draw({
@@ -96,8 +72,88 @@ const MapComponent = ({ type }: { type: string }) => {
       drawRef.current = draw;
 
       mapInstance.current.addInteraction(draw);
-      snapRef.current = new Snap({ source: source });
-      mapInstance.current.addInteraction(snapRef.current);
+
+      const selectInteraction = new Select({
+        condition: click,
+        layers: [vector],
+        multi: false,
+      });
+
+      selectInteractionRef.current = selectInteraction;
+
+      mapInstance.current.addInteraction(selectInteraction);
+
+      selectInteraction.on("select", (event) => {
+        const selected = event.selected;
+        const deselected = event.deselected;
+
+        deselected.forEach((feature) => {
+          highlightSource.removeFeature(feature);
+        });
+
+        selected.forEach((feature) => {
+          highlightVector.getSource()?.addFeature(feature);
+
+          mapInstance.current?.on("dblclick", (event) => {
+            event.preventDefault();
+            mapInstance.current?.getInteractions().forEach((interaction) => {
+              if (interaction instanceof interactionDoubleClickZoom) {
+                interaction.setActive(false);
+              }
+            });
+
+            const clonedFeature = feature.clone();
+
+            clonedFeature.setId(`clone-${feature.getId()}`);
+
+            highlightVector.getSource()?.addFeature(clonedFeature);
+
+            highlightSource.removeFeature(feature);
+
+            modifyRef.current = new Modify({
+              source: highlightSource,
+            });
+
+            mapInstance?.current.addInteraction(modifyRef.current);
+
+            modifyRef.current.on("modifystart", () => {
+              document.body.style.cursor = "pointer";
+            });
+
+            modifyRef.current.on("modifyend", () => {
+              document.body.style.cursor = "default";
+            });
+
+            mapInstance.current.on("dblclick", (event) => {
+              event.preventDefault();
+              const modifiedFeature = highlightSource.getFeatureById(
+                `clone-${feature.getId()}`
+              );
+
+              if (modifiedFeature) {
+                highlightSource.removeFeature(modifiedFeature);
+                source.removeFeature(feature);
+
+                modifiedFeature.setStyle(style);
+
+                source.addFeature(modifiedFeature);
+
+              }
+            });
+          });
+          // if (!isEditing) {
+          //   console.log("Removing modify interaction");
+          //   mapInstance.current?.getInteractions().forEach((interaction) => {
+          //     if (interaction instanceof interactionDoubleClickZoom) {
+          //       interaction.setActive(true);
+          //     }
+          //   });
+          // }
+        });
+      });
+
+      // snapRef.current = new Snap({ source: source });
+      // mapInstance.current.addInteraction(snapRef.current);
     }
 
     if (mapInstance.current && sourceRef.current) {
@@ -122,10 +178,95 @@ const MapComponent = ({ type }: { type: string }) => {
         drawRef.current = draw;
 
         mapInstance.current.addInteraction(draw);
-        snapRef.current = new Snap({
-          source: sourceRef.current,
+
+        const highlightSource = new VectorSource({ wrapX: false });
+        const highlightVector = new VectorLayer({
+          source: highlightSource,
+          style: selectedStyle,
         });
-        mapInstance.current.addInteraction(snapRef.current);
+
+        const vector = new VectorLayer({
+          source: sourceRef.current,
+          style: style,
+        });
+
+        const selectInteraction = new Select({
+          condition: click,
+          layers: [vector],
+          multi: false,
+        });
+
+        selectInteractionRef.current = selectInteraction;
+
+        mapInstance.current.addInteraction(selectInteraction);
+
+        selectInteraction.on("select", (event) => {
+          const selected = event.selected;
+          const deselected = event.deselected;
+
+          deselected.forEach((feature) => {
+            highlightSource.removeFeature(feature);
+          });
+
+          selected.forEach((feature) => {
+            highlightVector.getSource()?.addFeature(feature);
+
+            mapInstance.current?.on("dblclick", (event) => {
+              event.preventDefault();
+              mapInstance.current?.getInteractions().forEach((interaction) => {
+                if (interaction instanceof interactionDoubleClickZoom) {
+                  interaction.setActive(false);
+                }
+              });
+
+              const clonedFeature = feature.clone();
+
+              clonedFeature.setId(`clone-${feature.getId()}`);
+
+              highlightVector.getSource()?.addFeature(clonedFeature);
+
+              highlightSource.removeFeature(feature);
+
+              modifyRef.current = new Modify({
+                source: highlightSource,
+              });
+
+              mapInstance?.current.addInteraction(modifyRef.current);
+
+              modifyRef.current.on("modifystart", () => {
+                document.body.style.cursor = "pointer";
+              });
+
+              modifyRef.current.on("modifyend", () => {
+                document.body.style.cursor = "default";
+              });
+
+              mapInstance.current.on("dblclick", (event) => {
+                event.preventDefault();
+                const modifiedFeature = highlightSource.getFeatureById(
+                  `clone-${feature.getId()}`
+                );
+
+                if (modifiedFeature) {
+                  highlightSource.removeFeature(modifiedFeature);
+                  sourceRef.current.removeFeature(feature);
+
+                  modifiedFeature.setStyle(style);
+
+                  sourceRef.current.addFeature(modifiedFeature);
+                }
+              });
+            });
+            // if (!isEditing) {
+            //   console.log("Removing modify interaction");
+            //   mapInstance.current?.getInteractions().forEach((interaction) => {
+            //     if (interaction instanceof interactionDoubleClickZoom) {
+            //       interaction.setActive(true);
+            //     }
+            //   });
+            // }
+          });
+        });
       }
     }
   }, [type]);
